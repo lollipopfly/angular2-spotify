7553f6800c4dd291f06a21db1eff1f6b2b3b0197
lollipopfly <lollipop.fly@gmail.com>
2016-12-05 14:30:21 +0300

    make about & contact page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  title: string = 'About'
+  text: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, illo, unde! Ex, sunt? Omnis repellendus, repudiandae maxime ipsam nesciunt nulla, doloribus, deleniti accusantium natus saepe veniam odio, laboriosam facere dolorem.';
 
   constructor() { }
 
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  title: string = 'Contact'
+  text: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, illo, unde! Ex, sunt? Omnis repellendus, repudiandae maxime ipsam nesciunt nulla, doloribus, deleniti accusantium natus saepe veniam odio, laboriosam facere dolorem.';
 
   constructor() { }
 
   ngOnInit() {
+    console.log(21);
   }
 
 }
